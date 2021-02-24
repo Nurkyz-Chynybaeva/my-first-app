7c15b39bdab45fb97d460073a6186052d82ac0ba
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-02-24 11:56:58 +0600

    my first job with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+function Person(props) {
+  let smokes = <strong>Ne kurit</strong>
+  if (props.smokes) {
+    smokes = <strong>Kurit</strong>
+  }
+ 
+ return <div className="person">
+   <h1>{ props.name }</h1>
+   <div className="age">{ props.age } years old</div>
+   <p className="hobby">{ props.children }</p>
+   <strong>{ smokes }</strong>
+ </div>;
+}
+
+// ReactDOM.render(
+//  <div>
+   
+//  </div>,
+//  document.querySelector('body')
+// );
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Person name="Elon Musk" age={41}>Listens to <strong>music</strong></Person>
+   <Person name="Jeff Besos" age={55} smokes={true} />
+   <Person name="Bill Gates" age={56} smokes={true}  hobbies={["Singing", "Dancing"]}>Travels</Person>
+
     </div>
   );
 }
 
 export default App;
+
+
+
