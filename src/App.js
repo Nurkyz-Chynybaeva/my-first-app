1466af9ca89a6d74d08d067f603a01ab89865bf5
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-02-25 13:57:52 +0600

    sum a b

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Sum from './components/Sum';
+
+
 
 function App() {
   return(
   <div className= "App">
-
+<Sum a ="5" b = "14" />
 
 
   </div>
