e19e876a3f9333a16da4d2b3602194c7e2b60d4a
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-02-26 13:25:04 +0600

    Component Book

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Sum from './components/Sum';
 import RandomNumber from './components/RandomNumber';
 import BiggerNumber from './components/BiggerNumber';
 import Age from './components/Age';
+import Book from './components/Book';
 
 
 
@@ -13,6 +14,8 @@ function App() {
 <RandomNumber min = "1"  max = "5" />
 <BiggerNumber a = "5" b= "9"/>
 <Age year = "2002"/>
+<Book name = "Алхимик" autor = "Паулэ Коэльо" />
+
 
   </div>
 )}
---
+++ src/components/Book.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Book(props) {
+  return <div>
+    "{props.name}" by {props.autor} 
+  </div>
+  
+}
+export default Book;
+
+
