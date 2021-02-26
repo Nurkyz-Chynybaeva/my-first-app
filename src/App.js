1571c556f197f2e31322af2ae0e3d4268a442bad
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-02-26 13:20:49 +0600

    Age

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Sum from './components/Sum';
 import RandomNumber from './components/RandomNumber';
 import BiggerNumber from './components/BiggerNumber';
+import Age from './components/Age';
 
 
 
@@ -11,10 +12,9 @@ function App() {
 <Sum a ="5" b = "14" />
 <RandomNumber min = "1"  max = "5" />
 <BiggerNumber a = "5" b= "9"/>
-
+<Age year = "2002"/>
 
   </div>
-
 )}
 
 
--- src/components/Age.js
+++ src/components/Age.js
@@ -1,4 +1,13 @@
+import React from 'react';
 
+function Age(props) {
+return <div> I'm 
+   { 2021 - props.year} years old
+</div>
+  
+}
+
+export default Age;
 
 
 
